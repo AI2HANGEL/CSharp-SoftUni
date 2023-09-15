a791d4a6005e7ad80a8103479e72e629262750cf
Daniel Prandzhev <114281603+DPrandzhev@users.noreply.github.com>
2023-09-15 23:21:30 +0300

    Add files via upload

---
+++ FirstStepsInCSharp/BasketBallEquipment/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BasketBallEquipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int AnnualFee = int.Parse(Console.ReadLine());
+
+
+
+            double Sneakers = AnnualFee - (AnnualFee * 0.4);
+            double Team = Sneakers - (Sneakers * 0.2);
+            double Ball = Team * 0.25;
+            double Accesories = Ball * 0.2;
+
+            double Total = AnnualFee + Sneakers + Team + Ball + Accesories;
+            Console.WriteLine(Total);
+
+        }
+    }
+}
---
+++ FirstStepsInCSharp/Cinema/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeofstring = Console.ReadLine();
+            int numberofrows = int.Parse(Console.ReadLine());
+            int numberofcolumns = int.Parse(Console.ReadLine());
+            double income = 0.0;
+
+            double PremierePrice = 12.00;
+            double NormalPrice = 7.50;
+            double Discounted = 5.0;
+
+            if  (typeofstring == "Premiere")
+                {
+                income = PremierePrice * numberofcolumns * numberofrows;
+            }
+            else if (typeofstring == "Normal")
+            {
+                income = NormalPrice * numberofcolumns * numberofrows;
+            }
+            else if (typeofstring == "Discount")
+            {
+                income = Discounted * numberofcolumns * numberofrows;
+            }
+
+            Console.WriteLine("{0:f2} leva", income);
+        }
+    }
+}
