e6a3e35ca08dcdc8aa31019c417f84be21a8d8e2
Daniel Prandzhev <114281603+DPrandzhev@users.noreply.github.com>
2023-09-15 23:33:45 +0300

    Add files via upload

---
+++ FirstStepsInCSharp/TeachingMaterials/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TeachingMaterials
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int PacksOfPens = int.Parse(Console.ReadLine());
+            int PacksOfMarkers = int.Parse(Console.ReadLine());
+            int LitresOfCleaner = int.Parse(Console.ReadLine());
+            double PercentDiscount = double.Parse(Console.ReadLine()) / 100;
+
+            double PricePens = PacksOfPens * 5.8;
+            double PriceMarker = PacksOfMarkers * 7.2;
+            double PriceCleaner = LitresOfCleaner * 1.2;
+
+            double PriceAll = (PricePens + PriceMarker + PriceCleaner) - ((PricePens + PriceMarker + PriceCleaner) * PercentDiscount);
+            Console.WriteLine(PriceAll);
+
+        }   
+    }
+}
