4317b45a0337e0c392494ef5102cc39375ccfab2
Daniel Prandzhev <114281603+DPrandzhev@users.noreply.github.com>
2023-09-15 23:32:29 +0300

    Add files via upload

---
+++ FirstStepsInCSharp/Redecorating/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Redecorating
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nylon = int.Parse(Console.ReadLine());
+            int paint = int.Parse(Console.ReadLine());
+            int paint_sol = int.Parse(Console.ReadLine());
+            int work_hours = int.Parse(Console.ReadLine());
+
+            double nylon_sum = (nylon + 2) * 1.50;
+            double paint_sum = (paint * 1.1) * 14.50;
+            double paint_sol_sum = paint_sol * 5;
+            double bags = 0.4;
+
+            double total_sum = nylon_sum + paint_sum + paint_sol_sum + bags;
+            double workers_sum = (total_sum * 0.3) * work_hours;
+
+            Console.WriteLine(total_sum + workers_sum);
+        }
+    }
+}
