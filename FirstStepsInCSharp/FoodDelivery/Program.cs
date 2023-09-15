f85dc2449438c405bb28e7f47bb832f7f1088395
Daniel Prandzhev <114281603+DPrandzhev@users.noreply.github.com>
2023-09-15 23:26:49 +0300

    Add files via upload

---
+++ FirstStepsInCSharp/FoodDelivery/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FoodDelivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double ChickenMenuPrice = 10.35;
+            double FishMenuPrice = 12.40;
+            double VegMenuPrice = 8.15;
+
+            int AmountOfChickenMenu = int.Parse(Console.ReadLine());    
+            int AmountOfFishMenu = int.Parse(Console.ReadLine());
+            int AmountOfVegMenu = int.Parse(Console.ReadLine());
+            
+            double NetPrice = (AmountOfChickenMenu * ChickenMenuPrice) +
+                (AmountOfFishMenu * FishMenuPrice) +
+                (AmountOfVegMenu * VegMenuPrice);
+
+            double DesertPrice = NetPrice * 0.2;
+
+            double PriceAll = NetPrice + DesertPrice + 2.5;
+
+            Console.WriteLine(PriceAll);
+        }
+    }
+}
---
+++ FirstStepsInCSharp/hotelroom/Program.cs
@@ -0,0 +1,61 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        string month = Console.ReadLine();
+        int amountNights = int.Parse(Console.ReadLine());
+
+        double priceStudio = 0;
+        double priceApartment = 0;
+
+        switch (month)
+        {
+            case "May":
+            case "October":
+                if (amountNights <= 7)
+                {
+                    priceStudio = amountNights * 50;
+                }
+                else if (amountNights < 14)
+                {
+                    priceStudio = (amountNights * 50) * 0.95;
+                }
+                else
+                {
+                    priceStudio = (amountNights * 50) * 0.7;
+                }
+
+                priceApartment = amountNights * 65;
+                break;
+
+            case "June":
+            case "September":
+                if (amountNights > 14)
+                {
+                    priceStudio = (amountNights * 75.2) * 0.8;
+                }
+                else
+                {
+                    priceStudio = amountNights * 75.2;
+                }
+
+                priceApartment = amountNights * 68.70;
+                break;
+
+            case "July":
+            case "August":
+                priceStudio = amountNights * 76;
+                priceApartment = amountNights * 77;
+                break;
+        }
+
+        if (amountNights > 14)
+        {
+            priceApartment *= 0.9;
+        }
+
+        Console.WriteLine($"Apartment: {priceApartment:f2} lv.");
+        Console.WriteLine($"Studio: {priceStudio:f2} lv.");
+    }
+}
