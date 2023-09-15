3f153c46d8df2ee2bc660ad5ae20e5113276f5cc
Daniel Prandzhev <114281603+DPrandzhev@users.noreply.github.com>
2023-09-15 23:28:58 +0300

    Add files via upload

---
+++ FirstStepsInCSharp/MandatoryLiterature/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MandatoryLiterature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumberOfPages = int.Parse(Console.ReadLine());  
+            int Pages = int.Parse(Console.ReadLine());  
+            int NumberOfDays = int.Parse(Console.ReadLine());
+
+            int NumberOfHours = (NumberOfPages / Pages) / NumberOfDays;
+            Console.WriteLine(NumberOfHours);
+
+        }
+    }
+}
---
+++ FirstStepsInCSharp/journey/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double budget = double.Parse(Console.ReadLine());
+        string season = Console.ReadLine().ToLower();
+
+        string destination = "";
+        string holidayType = "";
+        double amountSpent = 0.0;
+
+        if (budget <= 100)
+        {
+            destination = "Bulgaria";
+            if (season == "summer")
+            {
+                holidayType = "Camp";
+                amountSpent = budget * 0.3;
+            }
+            else if (season == "winter")
+            {
+                holidayType = "Hotel";
+                amountSpent = budget * 0.7;
+            }
+        }
+        else if (budget <= 1000)
+        {
+            destination = "Balkans";
+            if (season == "summer")
+            {
+                holidayType = "Camp";
+                amountSpent = budget * 0.4;
+            }
+            else if (season == "winter")
+            {
+                holidayType = "Hotel";
+                amountSpent = budget * 0.8;
+            }
+        }
+        else
+        {
+            destination = "Europe";
+            holidayType = "Hotel";
+            amountSpent = budget * 0.9;
+        }
+
+        Console.WriteLine($"Somewhere in {destination}");
+        Console.WriteLine($"{holidayType} - {amountSpent:f2}");
+    }
+}
