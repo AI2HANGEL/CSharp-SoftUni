6cbf7cd7f279e12b09e898972ebd08327dc21a01
Daniel Prandzhev <114281603+DPrandzhev@users.noreply.github.com>
2023-09-15 23:19:47 +0300

    Add files via upload

---
+++ FirstStepsInCSharp/Aquarium/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Aquarium
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenghtcm = int.Parse(Console.ReadLine());
+            int widthcm = int.Parse(Console.ReadLine());
+            int heightcm = int.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine()) / 100;
+
+            double volumeliters = (lenghtcm * widthcm * heightcm) * 0.001;
+
+            double requiredliters = volumeliters * (1 - percentage);
+
+            string formattedRequiredLiters = string.Format("{0:N2}", requiredliters);
+
+            Console.WriteLine(formattedRequiredLiters);
+        }
+    }
+}
