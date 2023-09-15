f17dcfa51872a9ad02e4f588dcd8d4ddd7c83b92
Daniel Prandzhev <114281603+DPrandzhev@users.noreply.github.com>
2023-09-15 23:24:23 +0300

    Add files via upload

---
+++ FirstStepsInCSharp/DepositCalculator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DepositCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double DepositedAmount = double.Parse(Console.ReadLine());
+            int Months = int.Parse(Console.ReadLine());
+            double AnnualInterest = double.Parse(Console.ReadLine()) / 100;
+            double amount = DepositedAmount + Months * (DepositedAmount * AnnualInterest) / 12;
+            Console.WriteLine(amount);
+        }
+    }
+}
