b8926ec708aea2688619af5da439852ae36b8867
Daniel Prandzhev <daniel.prandzhev@gmail.com>
2023-10-23 20:55:08 +0300

    update

---
+++ ListsAndArrays/GaussTrick/Program.cs
@@ -0,0 +1,20 @@
+﻿List<int> numbers = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToList();
+
+while (numbers.Count > 1)
+{
+    
+    int sum = numbers.First() + numbers.Last();
+    Console.Write(sum + " ");
+
+    numbers.Remove(numbers.First());
+    numbers.Remove(numbers.Last());
+
+}
+
+if (numbers.Count == 1)
+{
+    Console.WriteLine(numbers[0]);
+}
---
+++ ListsAndArrays/MergingLists/Program.cs
@@ -0,0 +1,31 @@
+﻿List<int> numberSeqOne = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+List<int> numberSeqTwo = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+
+
+List<int> mergedList = MergeLists(numberSeqOne, numberSeqTwo);
+Console.WriteLine(string.Join(" ", mergedList));
+
+
+
+static List<int> MergeLists(List<int> list1, List<int> list2)
+{
+    List<int> mergedList = new List<int>();
+
+    int maxLength = Math.Max(list1.Count, list2.Count);
+
+    for (int i = 0; i < maxLength; i++)
+    {
+        if (i < list1.Count)
+        {
+            mergedList.Add(list1[i]);
+        }
+
+        if (i < list2.Count)
+        {
+            mergedList.Add(list2[i]);
+        }
+    }
+
+    return mergedList;
+}
