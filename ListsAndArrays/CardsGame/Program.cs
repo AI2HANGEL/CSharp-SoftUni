8991a1caea4857ff40d1174bd4bfe8c94f2d7ad5
Daniel Prandzhev <daniel.prandzhev@gmail.com>
2023-10-23 22:55:09 +0300

    Upload

---
+++ ListsAndArrays/CardsGame/Program.cs
@@ -0,0 +1,31 @@
+﻿List<int> playerOne = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+List<int> playerTwo = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+while (playerOne.Count > 0 && playerTwo.Count > 0)
+{
+    int playerOneCard = playerOne[0];
+    int playerTwoCard = playerTwo[0];
+
+    playerOne.RemoveAt(0);
+    playerTwo.RemoveAt(0);
+
+    if (playerOneCard > playerTwoCard)
+    {
+        playerOne.Add(playerOneCard);
+        playerOne.Add(playerTwoCard);
+    }
+    else if (playerTwoCard > playerOneCard)
+    {
+        playerTwo.Add(playerTwoCard);
+        playerTwo.Add(playerOneCard);
+    }
+}
+
+if (playerOne.Count > playerTwo.Count)
+{
+    Console.WriteLine($"First player wins! Sum: {playerOne.Sum()}");
+}
+else if (playerTwo.Count > playerOne.Count)
+{
+    Console.WriteLine($"Second player wins! Sum: {playerTwo.Sum()}");
+}
--- ListsAndArrays/_SumAdjascentEqualNumbers/Program.cs
+++ ListsAndArrays/_SumAdjascentEqualNumbers/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<int> NumberSequence = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+bool foundAdjacentEqualNumbers = true;
+
+while (foundAdjacentEqualNumbers)
+{
+    foundAdjacentEqualNumbers = false;
+
+    for (int i = 0; i < NumberSequence.Count - 1; i++)
+    {
+        if (NumberSequence[i] == NumberSequence[i + 1])
+        {
+            int sum = NumberSequence[i] + NumberSequence[i + 1];
+            NumberSequence[i] = sum;
+            NumberSequence.RemoveAt(i + 1);
+            foundAdjacentEqualNumbers = true;
+        }
+    }
+}
+
+Console.WriteLine(string.Join(" ", NumberSequence));
