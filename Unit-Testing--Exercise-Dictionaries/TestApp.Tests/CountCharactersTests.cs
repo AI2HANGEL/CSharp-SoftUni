4687fb806db320af1267b258093df7a9a6c0b1cc
Daniel Prandzhev <daniel.prandzhev@gmail.com>
2023-11-29 14:19:34 +0200

    update

---
+++ Unit-Testing--Exercise-Dictionaries/TestApp.Tests/CountCharactersTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+
+namespace TestApp.Tests;
+
+public class CountCharactersTests
+{
+    [Test]
+    public void Test_Count_WithEmptyList_ShouldReturnEmptyString()
+    {
+        // Arrange
+        List<string> input = new();
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_Count_WithNoCharacters_ShouldReturnEmptyString()
+    {
+        // Arrange
+        List<string> input = new() { "", "", "", "" };
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_Count_WithSingleCharacter_ShouldReturnCountString()
+    {
+        // Arrange
+        List<string> input = new() { "aa", "aaa", "a", "aaaa" };
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("a -> 10"));
+    }
+
+    [Test]
+    public void Test_Count_WithMultipleCharacters_ShouldReturnCountString()
+    {
+        // Arrange
+        List<string> input = new() { "aa", "bbb", "c", "aaaa" };
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("a -> 6\r\nb -> 3\r\nc -> 1"));
+    }
+
+    [Test]
+    public void Test_Count_WithSpecialCharacters_ShouldReturnCountString()
+    {
+        // Arrange
+        List<string> input = new() { "aa", "$$", "aaaa" };
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("a -> 6\r\n$ -> 2"));
+    }
+}
